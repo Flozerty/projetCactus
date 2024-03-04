8b04093a026e5c104bf97abbf6832607cbe3b41d
Flozerty <floris.louerat@gmail.com>
2024-03-04 19:13:11 +0100

    end project

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,3 +69,14 @@ const scrollActive = () => {
 window.addEventListener('scroll', scrollActive);
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2500,
+  delay: 300,
+})
+
+sr.reveal(`.home__img, .new__data, .care__img, .contact__content, .footer`)
+sr.reveal(`.home__data, .care__list, .contact__img`, { delay: 500 })
+sr.reveal(`.new__card`, { delay: 500, interval: 150 })
+sr.reveal(`.shop__card`, { interval: 100 })
